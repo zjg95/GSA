1b01a4dc4033ca14744b8cdbcf47dce2762830ed
David Acker <David@wireless-10-147-221-52.public.utexas.edu>
2015-10-12 16:13:12 -0500

    Added push long exposure using tutorial: http://www.freshconsulting.com/create-drag-and-drop-uitableview-swift/

--- GSA/EmployeeTableViewController.swift
+++ GSA/EmployeeTableViewController.swift
@@ -24,6 +24,10 @@ class EmployeeTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let longpress = UILongPressGestureRecognizer(target: self, action: "longPressGestureRecognized:")
+        
+        tableView.addGestureRecognizer(longpress)
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -129,5 +133,103 @@ class EmployeeTableViewController: UITableViewController {
             tableView.deleteRowsAtIndexPaths([index], withRowAnimation: .Bottom)
         }
     }
+    
+    //Function to move cells via a long press
+    func longPressGestureRecognized(gestureRecognizer: UIGestureRecognizer) {
+        let longPress = gestureRecognizer as! UILongPressGestureRecognizer
+        let state = longPress.state
+        var locationInView = longPress.locationInView(tableView)
+        var indexPath = tableView.indexPathForRowAtPoint(locationInView)
+        
+        struct My {
+            
+            static var cellSnapshot : UIView? = nil
+            
+        }
+        struct Path {
+            
+            static var initialIndexPath : NSIndexPath? = nil
+            
+        }
+        
+        switch state {
+            case UIGestureRecognizerState.Began:
+                if indexPath != nil {
+                    Path.initialIndexPath = indexPath
+                    let cell = tableView.cellForRowAtIndexPath(indexPath!) as UITableViewCell!
+                    My.cellSnapshot  = snapshopOfCell(cell)
+                    var center = cell.center
+                
+                    My.cellSnapshot!.center = center
+                
+                    My.cellSnapshot!.alpha = 0.0
+                
+                    tableView.addSubview(My.cellSnapshot!)
+                
+                    UIView.animateWithDuration(0.25, animations: { () -> Void in
+                        center.y = locationInView.y
+                    
+                        My.cellSnapshot!.center = center
+                    
+                        My.cellSnapshot!.transform = CGAffineTransformMakeScale(1.05, 1.05)
+                    
+                        My.cellSnapshot!.alpha = 0.98
+                    
+                        cell.alpha = 0.0
+                    
+                        }, completion: { (finished) -> Void in
+                        
+                            if finished {
+                            
+                                cell.hidden = true
+                            
+                            }
+                        
+                    })
+                
+                }
+            case UIGestureRecognizerState.Changed:
+                var center = My.cellSnapshot!.center
+                center.y = locationInView.y
+                My.cellSnapshot!.center = center
+                if ((indexPath != nil) && (indexPath != Path.initialIndexPath)) {
+                    swap(&employeeList[indexPath!.row], &employeeList[Path.initialIndexPath!.row])
+                    tableView.moveRowAtIndexPath(Path.initialIndexPath!, toIndexPath: indexPath!)
+                    Path.initialIndexPath = indexPath
+                }
+            default:
+                let cell = tableView.cellForRowAtIndexPath(Path.initialIndexPath!) as UITableViewCell!
+                cell.hidden = false
+                cell.alpha = 0.0
+                UIView.animateWithDuration(0.25, animations: { () -> Void in
+                    My.cellSnapshot!.center = cell.center
+                    My.cellSnapshot!.transform = CGAffineTransformIdentity
+                    My.cellSnapshot!.alpha = 0.0
+                    cell.alpha = 1.0
+                    }, completion: { (finished) -> Void in
+                        if finished {
+                            Path.initialIndexPath = nil
+                            My.cellSnapshot!.removeFromSuperview()
+                            My.cellSnapshot = nil
+                        }
+                })
+        }
+    }
+    
+    func snapshopOfCell(inputView: UIView) -> UIView {
+        UIGraphicsBeginImageContextWithOptions(inputView.bounds.size, false, 0.0)
+        inputView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext() as UIImage
+        UIGraphicsEndImageContext()
+        let cellSnapshot : UIView = UIImageView(image: image)
+        cellSnapshot.layer.masksToBounds = false
+        cellSnapshot.layer.cornerRadius = 0.0
+        cellSnapshot.layer.shadowOffset = CGSizeMake(-5.0, 0.0)
+        cellSnapshot.layer.shadowRadius = 5.0
+        cellSnapshot.layer.shadowOpacity = 0.4
+        return cellSnapshot
+    }
+    
+        
 
 }
