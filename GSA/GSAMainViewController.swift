e8ebe29dcce8e5442216adeb942e6db132f0d293
Zachary Goodman <zjgoodman@utexas.edu>
2015-12-03 20:25:41 -0600

    removed demo schedule and employees

--- GSA/GSAMainViewController.swift
+++ GSA/GSAMainViewController.swift
@@ -16,7 +16,7 @@ class GSAMainViewController: UITabBarController {
     
     let staff: Staff = Staff()
     let week: Week = Week()
-    var scheduleList: [Schedule]!
+    var scheduleList: [Schedule] = []
     
     // -------
     // methods
@@ -27,22 +27,22 @@ class GSAMainViewController: UITabBarController {
 
         // Do any additional setup after loading the view.
         
-        staff.append(Employee(firstName: "Gus", lastName: "Ortiz", position: "Manager"))
-        staff.append(Employee(firstName: "Juan", lastName: "Rivas", position: "Shift Leader"))
-        staff.append(Employee(firstName: "Troy", lastName: "Rowden", position: "Shift Leader"))
-        staff.append(Employee(firstName: "John", lastName: "Larkin", position: "Employee"))
+//        staff.append(Employee(firstName: "Gus", lastName: "Ortiz", position: "Manager"))
+//        staff.append(Employee(firstName: "Juan", lastName: "Rivas", position: "Shift Leader"))
+//        staff.append(Employee(firstName: "Troy", lastName: "Rowden", position: "Shift Leader"))
+//        staff.append(Employee(firstName: "John", lastName: "Larkin", position: "Employee"))
+//        
+//        let schedule: Schedule = Schedule(name: "Demo Schedule", staff: staff.copy(), week: week.copy())
+//        
+//        schedule.append(Shift(timeStart: Time(hour: 9, minutes: 0), timeEnd: Time(hour: 10, minutes: 0), day: 0))
+//        schedule.append(Shift(timeStart: Time(hour: 12, minutes: 0), timeEnd: Time(hour: 13, minutes: 0), day: 1))
+//        schedule.append(Shift(timeStart: Time(hour: 10, minutes: 0), timeEnd: Time(hour: 17, minutes: 0), day: 2))
+//        schedule.append(Shift(timeStart: Time(hour: 6, minutes: 0), timeEnd: Time(hour: 10, minutes: 0), day: 3))
+//        schedule.append(Shift(timeStart: Time(hour: 11, minutes: 0), timeEnd: Time(hour: 17, minutes: 0), day: 4))
+//        schedule.append(Shift(timeStart: Time(hour: 14, minutes: 0), timeEnd: Time(hour: 16, minutes: 0), day: 5))
+//        schedule.append(Shift(timeStart: Time(hour: 13, minutes: 0), timeEnd: Time(hour: 20, minutes: 0), day: 6))
         
-        let schedule: Schedule = Schedule(name: "Demo Schedule", staff: staff.copy(), week: week.copy())
-        
-        schedule.append(Shift(timeStart: Time(hour: 9, minutes: 0), timeEnd: Time(hour: 10, minutes: 0), day: 0))
-        schedule.append(Shift(timeStart: Time(hour: 12, minutes: 0), timeEnd: Time(hour: 13, minutes: 0), day: 1))
-        schedule.append(Shift(timeStart: Time(hour: 10, minutes: 0), timeEnd: Time(hour: 17, minutes: 0), day: 2))
-        schedule.append(Shift(timeStart: Time(hour: 6, minutes: 0), timeEnd: Time(hour: 10, minutes: 0), day: 3))
-        schedule.append(Shift(timeStart: Time(hour: 11, minutes: 0), timeEnd: Time(hour: 17, minutes: 0), day: 4))
-        schedule.append(Shift(timeStart: Time(hour: 14, minutes: 0), timeEnd: Time(hour: 16, minutes: 0), day: 5))
-        schedule.append(Shift(timeStart: Time(hour: 13, minutes: 0), timeEnd: Time(hour: 20, minutes: 0), day: 6))
-        
-        scheduleList = [schedule]
+//        scheduleList.append(schedule)
         
         if let nav = self.viewControllers![0] as? UINavigationController {
             if let schedule = nav.viewControllers.first as? ScheduleListTableViewController {
